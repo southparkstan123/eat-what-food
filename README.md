6968d4082e860d914583d301cbc09a72309a7226
Tik Sang Wong <tiksangwong@Tikde-MacBook-Pro.local>
2017-12-11 14:19:08 +0800

    Init config for project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # eat-what-food
+Start the app by following steps:
+1. Clone the repository from Github
+2. npm install 
+3. Copy the ".example.env" file and modify the configuration such as facebook api_key, database configuration for it, save the file as ".env"
+4. npm start
 Enjoy!
---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const hb = require('express-handlebars');
+const bodyParser = require('body-parser');
+
+app.engine('handlebars', hb({ 
+    defaultLayout: 'main'
+}));
+
+//Import the environment configs
+require('dotenv').config();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.get('/',(req,res) =>{
+    res.render('partials/chatroom');
+})
+
+app.get('*', function(req, res){
+    res.status(404).send('Page not found');
+});
+
+app.set('view engine', 'handlebars');
+app.listen(8080);
