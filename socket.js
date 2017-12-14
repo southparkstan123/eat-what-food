7b5e11e1e36f7bcc1384deb1c718305899b6f86d
Tik Sang Wong <tiksangwong@Tikde-MacBook-Pro.local>
2017-12-14 09:08:48 +0800

    Init facebook social login

--- socket.js
+++ socket.js
@@ -2,26 +2,42 @@ const session = require("./session");
 const socketIOSession = require("socket.io.session");
 const client = require('./database');
 
+client.once('ready', function() {
+    // Flush Redis DB
+    client.flushdb();
+
+});
+
 module.exports = (server)=>{
     const io = require('socket.io')(server);
     
     //"sessionSettings" same config as http-session used 
-    //const socketSession = socketIOSession(session.settings); 
+    const socketSession = socketIOSession(session.settings); 
     
-    //io.use(socketSession.parser);
+    io.use(socketSession.parser);
 
-    // io.use((socket, next)=>{
-    //     if(!socket.session.passport){
-    //         socket.disconnect();
-    //     }else{
-    //         next();
-    //     }
-    // });
+    io.use((socket, next)=>{
+        if(!socket.session.passport){
+            socket.disconnect();
+        }else{
+            next();
+        }
+    });
     
-    io.on('connection',(socket)=>{
-        //socket.session.passport
-        // socket.emit('show_who_logged_in',socket.session.passport.user);
+    io.on('connection',(socket) => {
 
+        socket.emit('user_enter_chatroom',socket.session.passport.user);
+
+        socket.on('load_users_in_chatroom_from_redis', () =>{
+            client.lrange('online_user_list',0, -1, (err, reply)=>{
+                if(reply){
+                    let online_user_list = reply.map((message) => {
+                        return JSON.parse(message);
+                    })
+                    io.emit('display_userlist', online_user_list);
+                }
+            });
+        });
 
         socket.on('load_messages_from_redis', () => {
             client.lrange('message_list',0, -1, (err, reply)=>{
@@ -29,27 +45,52 @@ module.exports = (server)=>{
                     let message_list = reply.map((message) => {
                         return JSON.parse(message);
                     })
-                    socket.emit('display_all_messages', message_list);
+                    io.emit('display_all_messages', message_list);
                 }
             });
         });
 
-        socket.on('send_message',(content)=>{
+        socket.on('send_message',(content) => {
             let messageObj = {
-                username: 'Dickson Wong',//socket.session.passport.user
-                userid: '167',//socket.session.passport.id
+                username: socket.session.passport.user.profile.displayName,
+                userid: socket.session.passport.user.profile.id,
                 content: content,
                 date: new Date().getTime()
             };
     
             client.rpush('message_list', JSON.stringify(messageObj) , (err, reply)=>{
                 if(reply){
-                    console.log(reply);
-                    console.log('message saved');
-                    socket.emit('display_sent_message', messageObj);
+                    io.emit('display_sent_message', messageObj);
                 }
             });
         });
+
+        socket.on('user_leave_chatroom',(userID) =>{            
+            client.lrange('online_user_list',0, -1, (err, reply)=>{
+                if(reply){
+                    let old_user_list = reply.map((user) => {
+                        return JSON.parse(user);
+                    });
+                    console.log(old_user_list);
+                    let leave_user = old_user_list.filter((user)=>{
+                        return user.userID == userID;
+                    });
+                    console.log(JSON.stringify(leave_user[0]));
+                    client.lrem('online_user_list', 0, JSON.stringify(leave_user[0]), (err,reply) =>{
+                        if(reply){
+                            client.lrange('online_user_list',0, -1, (err, reply)=>{
+                                if(reply){
+                                    let new_user_list = reply.map((user) => {
+                                        return JSON.parse(user);
+                                    });
+                                    io.emit('display_userlist', new_user_list);
+                                }
+                            });
+                        }
+                    })
+                }
+            });
+        })
     });
 
     return io;
