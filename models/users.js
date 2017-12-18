ebfc5c5d0f45a6d2f600623fea67942e6c00e490
Tik Sang Wong <tiksangwong@Tikde-MacBook-Pro.local>
2017-12-18 09:52:12 +0800

    Modify user mopdel

--- models/users.js
+++ models/users.js
@@ -1,16 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var users = sequelize.define('users', {
-    userName: DataTypes.STRING
+    userName: DataTypes.STRING,
+    facebookId: DataTypes.BIGINT
   });
 
   users.associate = function (models) {
     // associations can be defined here
     users.hasMany(models.chatrooms, {
-      foreignKey: "createdBy", sourceKey: "id"
+      foreignKey: "createdBy", sourceKey: "id", as: "creates"
     })
     users.hasMany(models.userChatrooms, {
-      foreignKey: "userId", sourceKey: "id"
+      foreignKey: "userId", sourceKey: "id", as: "invited"
     })
   }
 
